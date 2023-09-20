4cbe9d23a8fe7409433977fa30a671acd0969675
lovi6754 <133924252+lovi6754@users.noreply.github.com>
2023-09-20 06:33:08 +0530

    Update data.py

--- cache/data.py
+++ cache/data.py
@@ -205,13 +205,13 @@ RAID = [
 ]
 
 
-GRP = "@TheSupportChat|@MELODY_QUEEN20|@Shubhanshutya|Timesisnotwaiting|MELODY_QUEEN20|Shubhanshutya"
+GRP = "@loveguruoo|@RICH_OF_HEART|@Shubhanshutya|Timesisnotwaiting|MELODY_QUEEN20|Shubhanshutya"
 
-GROUP = [-1001521704453, -1001410362208]
+GROUP = [-1001521904453, -1001410762208]
 
 VERIFIED_USERS = [936481432, 1669178360, 1941391496, 1709495698, 1920507972, 1450303652]
 
-Owners = "@Timesisnotwaiting|@MELODY_QUEEN20|Zaid"
+Owners = "@loveguruo |loveguruo |lovi"
 
 PORM = [
         "https://telegra.ph/file/9bcc076fd81dfe3feb291.mp4",
